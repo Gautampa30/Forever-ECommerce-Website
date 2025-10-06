469c60aed00e83bf22dd831e985aae9f0aa9d546
Gautampa30 <gautampa30@gmail.com>
2025-10-06 13:47:45 +0530

    fix: use correct sendgrid plugin import for email

--- backend/config/emailConfig.js
+++ backend/config/emailConfig.js
@@ -1,12 +1,10 @@
 const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  auth: {
-    user: "apikey", // This is literally the string "apikey"
-    pass: process.env.SENDGRID_API_KEY, // Use your API key as password
-  },
-});
+const transporter = nodemailer.createTransport(
+  sgTransport({
+    apiKey: process.env.SENDGRID_API_KEY,
+  })
+);
 
 module.exports = transporter;
